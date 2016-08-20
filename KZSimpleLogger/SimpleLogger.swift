7bd9f543b0af9203ea287fd2bce74a46b5ae3aa4
Shane Whitehead <obishiro@gmail.com>
2016-08-20 14:36:30 +1000

    Initial commit

---
+++ KZSimpleLogger/SimpleLogger.swift
@@ -0,0 +1,65 @@
+//
+//  SimpleLogger.swift
+//  KZSimpleLogger
+//
+//  Created by Shane Whitehead on 20/8/16.
+//  Copyright © 2016 KaiZen. All rights reserved.
+//
+
+import Foundation
+
+public struct SimpleLogger {
+	
+	public enum LogType: String {
+		case info = "💡"
+		case warn = "⚠️"
+		case error = "🔥"
+		case debug = "🐞"
+	}
+	
+	public static var shared: SimpleLogger = SimpleLogger()
+	
+	var dateFormat: DateFormatter {
+		let df = DateFormatter()
+		df.dateStyle = .short
+		df.timeStyle = .short
+		return df
+	}
+	
+	public func log(_ type: LogType, message: String, function: String = #function, line: Int = #line, file: String = #file, date: Date = Date()) {
+		let fileName = (file as NSString).lastPathComponent
+		print("\(type)|[\(dateFormat.string(from: date)):\(fileName):\(function)@\(line)] \(message)")
+	}
+	
+	public func debug(message: String, function: String = #function, line: Int = #line, file: String = #file, date: Date = Date()) {
+		log(.debug, message: message, function: function, line: line, file: file, date: date)
+	}
+	
+	public func info(message: String, function: String = #function, line: Int = #line, file: String = #file, date: Date = Date()) {
+		log(.info, message: message, function: function, line: line, file: file, date: date)
+	}
+	
+	public func warn(message: String, function: String = #function, line: Int = #line, file: String = #file, date: Date = Date()) {
+		log(.warn, message: message, function: function, line: line, file: file, date: date)
+	}
+	
+	public func error(message: String, function: String = #function, line: Int = #line, file: String = #file, date: Date = Date()) {
+		log(.error, message: message, function: function, line: line, file: file, date: date)
+	}
+}
+
+public func log(info message: String, function: String = #function, line: Int = #line, file: String = #file, date: Date = Date()) {
+	SimpleLogger.shared.info(message: message, function: function, line: line, file: file, date: date)
+}
+
+public func log(debug message: String, function: String = #function, line: Int = #line, file: String = #file, date: Date = Date()) {
+	SimpleLogger.shared.debug(message: message, function: function, line: line, file: file, date: date)
+}
+
+public func log(warn message: String, function: String = #function, line: Int = #line, file: String = #file, date: Date = Date()) {
+	SimpleLogger.shared.warn(message: message, function: function, line: line, file: file, date: date)
+}
+
+public func log(error message: String, function: String = #function, line: Int = #line, file: String = #file, date: Date = Date()) {
+	SimpleLogger.shared.error(message: message, function: function, line: line, file: file, date: date)
+}
